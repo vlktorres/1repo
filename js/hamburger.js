59f5dd0f5a46e95f88eb5807972cc03cd5543b81
vlktorres <valekaty27@gmail.com>
2021-09-29 03:51:32 -0500

    6th responsive

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
-const sidebar = document.querySelector(".sidenav");
-const navLink = document.querySelectorAll(".iconLinks");
+const navMenu = document.querySelector(".sidenav");
+const navLink = document.querySelectorAll(".iconLinks a");
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-    sidebar.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
 
 function closeMenu() {
     hamburger.classList.remove("active");
-    sidebar.classList.remove("active");
+    navMenu.classList.remove("active");
 }
