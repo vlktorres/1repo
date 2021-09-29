84edc1f3fc80671b9bf241be63baaee45f3eeb50
vlktorres <valekaty27@gmail.com>
2021-09-29 03:48:11 -0500

    responsive + js

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,16 @@
-function openNav() {
-  document.getElementsByClassName("sidenav").style.width = "100%";
-  document.getElementsByClassName("rightCont").style.marginLeft = "100%";
+const hamburger = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidenav");
+const navLink = document.querySelectorAll(".iconLinks");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    sidebar.classList.toggle("active");
 }
 
-function closeNav() {
-  document.getElementsByClassName("sidenav").style.width = "0";
-  document.getElementsByClassName("rightCont").style.marginLeft= "0";
+function closeMenu() {
+    hamburger.classList.remove("active");
+    sidebar.classList.remove("active");
 }
